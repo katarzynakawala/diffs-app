434d41caf278c73824a014bb60efd15e6785277f
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-22 13:00:46 +0100

    creation of db connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dns", "web:pass@/diffsapp?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	//logger for writting information messages
@@ -21,6 +25,13 @@ func main() {
 
 	//logger for writting error messages
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	//new instance of application with dependencies
 	app := &application{
@@ -36,6 +47,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err!= nil {
+		return nil, err
+	}
+	return db, nil
 }
