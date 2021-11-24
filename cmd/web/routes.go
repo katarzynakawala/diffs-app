17dc5d0a4b7260aa0097c6390e79897fd584b2d0
Katarzyna Kawala <kat.kawala@gmail.com>
2021-11-24 20:57:50 +0100

    adding middleware chains

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	//middleware chain 
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -11,5 +18,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return app.logRequest((mux))
+	return standardMiddleware.Then((mux))
 }
