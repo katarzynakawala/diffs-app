53c72b055cc5e7d96c75d16f20f83b81f92aa137
Katarzyna Kawala <kat.kawala@gmail.com>
2021-12-03 00:20:39 +0100

    adding more tests handler and middleware

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	//dummy http.Request
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ping(rr, r)
+	rs := rr.Result()
+
+	if rs.StatusCode != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	}
+
+	//checking response body written by the handler
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+}
