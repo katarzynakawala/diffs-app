3468cb7150850e4e36d47f5b025905fc2d2b9f81
Katarzyna Kawala <kat.kawala@gmail.com>
2021-12-04 11:14:01 +0100

    e2e test and helpers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
+	ts := newTestserver(t, app.routes())
+	defer ts.Close()
 
-	//dummy http.Request
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	ping(rr, r)
-	rs := rr.Result()
-
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	//checking response body written by the handler
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
 	}
 
 	if string(body) != "OK" {
